acae5ab79495ff61e95192dcc27de77a56d3dc58
Skhan021 <138653770+Skhan021@users.noreply.github.com>
2023-11-05 17:38:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -55,6 +55,9 @@ async def start(client, message):
         m=await message.reply_text("👀")
         await asyncio.sleep(1)
         await m.delete()
+        madflix = await message.reply_sticker("CAACAgIAAxkBAAEKsGBlR4DcGRmmBvcVT2JDDQhs-Vj6RAACVAADQbVWDGq3-McIjQH6MwQ")
+    await asyncio.sleep(2)
+    await madflix.delete()
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
