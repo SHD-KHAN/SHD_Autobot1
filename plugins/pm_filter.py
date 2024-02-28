4dcd90ea134d0594ba0f66781bac273887a04349
SHD KHAN <138653770+SHD-KHAN@users.noreply.github.com>
2024-02-28 20:44:27 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -71,9 +71,7 @@ async def give_filter(client, message):
 
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
-   l = content = message.text
-   await asyncio.sleep(1)
-    await l.delete()
+    content = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
