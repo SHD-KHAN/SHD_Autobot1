8e411e14f4c48a75041d47244d40049e8264a787
SHD KHAN <138653770+SHD-KHAN@users.noreply.github.com>
2024-02-29 21:14:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -84,10 +84,7 @@ async def pm_text(bot, message):
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
     )
-        await asyncio.sleep(10)
-    logger.info("Message deleted")
-    await message.delete()
-
+        
 @Client.on_callback_query(filters.regex(r"^next"))
 async def next_page(bot, query):
     ident, req, key, offset = query.data.split("_")
